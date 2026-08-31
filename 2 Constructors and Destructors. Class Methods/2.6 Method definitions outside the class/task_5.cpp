6936396ea2eacd305467c58227970cc29929efea
smkymcpt <smokey_mc_pot_@outlook.com>
2026-08-31 18:04:01 +0500

    added : 2.6 task_5 solution

---
+++ 2 Constructors and Destructors. Class Methods/2.6 Method definitions outside the class/task_5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class Wallet {
+public:
+	Wallet() = delete;
+	Wallet(int volume) : m_volume{volume} {};
+
+	void add(int vol);
+	int get_volume() const;
+
+private:
+	int m_volume{0};
+	bool check_volume(int x);
+};
+
+bool Wallet::check_volume(int x) {
+	return x >= 0;
+}
+
+void Wallet::add(int vol) {
+	if (check_volume(vol)) {
+		m_volume += vol;
+	}
+}
+
+int Wallet::get_volume() const {
+	return m_volume;
+}
+
+int main() {
+	Wallet wallet{544653}; 
+	int vl = wallet.get_volume();
+	return 0;
+}
