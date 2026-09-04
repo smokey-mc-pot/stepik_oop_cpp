d2ab25e4ffc83ad906c4fc56d5c632a886d90a0b
smkymcpt <smokey_mc_pot_@outlook.com>
2026-09-04 16:54:29 +0500

    added : 2.9 task_7 solution

---
+++ 2 Constructors and Destructors. Class Methods/2.9 Const member functions. The mutable keyword/task_7.cpp
@@ -0,0 +1,55 @@
+#include <cstring>
+
+class GamePole {
+public:
+	GamePole(int rows, int cols) : m_rows{rows}, m_cols{cols} {
+        if (rows > 0 && cols > 0) {
+            m_pole = new char[rows * cols];
+            std::memset(m_pole, 0, rows * cols * sizeof(char));
+        }
+	}
+
+	GamePole(const GamePole& other) : m_rows{other.m_rows}, m_cols{other.m_cols} {
+        if (other.m_pole) {
+            m_pole = new char[m_rows * m_cols];
+            std::memcpy(m_pole, other.m_pole, m_rows * m_cols * sizeof(char));
+        }
+    }
+
+	~GamePole() {
+        delete[] m_pole;
+    }
+
+	void set_item(int row, int col, char value) const {
+		if (row >= 0 && row < m_rows && col >= 0 && col < m_cols && m_pole != nullptr) {
+			m_pole[row * m_cols + col] = value;
+		}
+	}
+
+	char get_item(int row, int col) const {
+		if (row >= 0 && row < m_rows && col >= 0 && col < m_cols && m_pole != nullptr) {
+			return m_pole[row * m_cols + col];
+		}
+	}
+
+	const char* get_pole() const {
+		return m_pole;
+	}
+
+	void get_size(int& rows, int& cols) const {
+		rows = m_rows;
+		cols = m_cols;
+	}
+
+private:
+	int m_rows{0}, m_cols{0};
+	char* m_pole{nullptr};
+};
+
+int main() {
+	GamePole gp{10, 7};
+	gp.set_item(3, 5, '#');
+	gp.set_item(4, 2, '*');
+	gp.set_item(4, 4, '*');
+	return 0;
+}
